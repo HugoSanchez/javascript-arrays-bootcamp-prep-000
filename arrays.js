b67a622599b3c317abe331488a52108316a832d5
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 10:05:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,4 +8,6 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}

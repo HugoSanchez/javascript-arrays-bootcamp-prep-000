9da3e29d1e0cf36a3e1d28a88077200af69608ac
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 10:16:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array = array.shift()
+  return array
+}

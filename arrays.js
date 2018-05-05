06d93ea38d8c7763a2a17478fe36a1950b400b82
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-05 10:44:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,3 +41,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 
+function removeElementFromEndOfArray(array){
+  array = array.slice(0, array.length -1)
+  return array
+}
